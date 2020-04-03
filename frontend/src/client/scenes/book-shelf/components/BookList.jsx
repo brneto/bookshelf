18e9ba94ad780405523b6bafe1dbc600625d7ec6
Berchris Requiao <berchris.requiao@ericsson.com>
2020-04-03 10:11:26 +0100

    Add filter and sort

--- frontend/src/client/scenes/book-shelf/components/BookList.jsx
+++ frontend/src/client/scenes/book-shelf/components/BookList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
@@ -8,16 +8,46 @@ const
     onEdit: PropTypes.func.isRequired,
     onDelete: PropTypes.func.isRequired,
   },
-  BookList = ({ books, onEdit, onDelete }) => (
-    <>
+  BookList = ({ books, onEdit, onDelete }) => {
+    const
+      [sortBy, setSortBy] = useState(''),
+      [filterBy, setFilterBy] = useState(''),
+      [filter, setFilter] = useState(''),
+      handleSortBy = (event) => console.log(event),
+      handleFilterBy = (event) => console.log(event),
+      handleSearch = (event) => console.log(event);
+
+    return (<>
+      <tr>
+        <td>
+          <label>Sort by:
+            <select value={sortBy} onBlur={handleSortBy}>
+              <option value="">--Sort By--</option>
+              <option value="title">Title</option>
+              <option value="author">Author</option>
+            </select>
+          </label>
+        </td>
+        <td>
+          <label>Sort by:
+            <select value={filterBy} onBlur={handleFilterBy}>
+              <option value="">--Filter By--</option>
+              <option value="title">Title</option>
+              <option value="author">Author</option>
+            </select>
+          </label>
+        </td>
+        <td><input type="text" value={filter} /></td>
+        <td><button onChange={handleSearch}>Search</button></td>
+      </tr>
       {books.map(({ id, ...rest }) => (
         <Book key={id} {...rest}
           onEdit={() => onEdit(id)}
           onDelete={() => onDelete(id)}
         />
       ))}
-    </>
-  );
+    </>);
+  };
 
 BookList.propTypes = propTypes;
 
